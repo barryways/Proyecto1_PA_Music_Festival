5d382da4f2db29ad97832eb22ba8124d80405b74
barryways <93161779+barryways@users.noreply.github.com>
2022-10-11 16:06:11 -0600

    UI changes initial

--- FrontEnd.h
+++ FrontEnd.h
@@ -34,6 +34,19 @@ namespace PAProyecto1CarlosDiego {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ pnlLateral;
+	protected:
+	private: System::Windows::Forms::Panel^ pnlSubMenu;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ btnMedios;
+	private: System::Windows::Forms::Panel^ pnlLogo;
+	private: System::Windows::Forms::Panel^ pnlReproductor;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::ListBox^ listBox2;
 
 	private:
 		/// <summary>
@@ -48,17 +61,225 @@ namespace PAProyecto1CarlosDiego {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(FrontEnd::typeid));
+			this->pnlLateral = (gcnew System::Windows::Forms::Panel());
+			this->pnlSubMenu = (gcnew System::Windows::Forms::Panel());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->btnMedios = (gcnew System::Windows::Forms::Button());
+			this->pnlLogo = (gcnew System::Windows::Forms::Panel());
+			this->pnlReproductor = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
+			this->pnlLateral->SuspendLayout();
+			this->pnlSubMenu->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// pnlLateral
+			// 
+			this->pnlLateral->AutoScroll = true;
+			this->pnlLateral->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->pnlLateral->Controls->Add(this->pnlSubMenu);
+			this->pnlLateral->Controls->Add(this->btnMedios);
+			this->pnlLateral->Controls->Add(this->pnlLogo);
+			this->pnlLateral->Dock = System::Windows::Forms::DockStyle::Left;
+			this->pnlLateral->Location = System::Drawing::Point(0, 0);
+			this->pnlLateral->Name = L"pnlLateral";
+			this->pnlLateral->Size = System::Drawing::Size(250, 649);
+			this->pnlLateral->TabIndex = 0;
+			// 
+			// pnlSubMenu
+			// 
+			this->pnlSubMenu->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(18)), static_cast<System::Int32>(static_cast<System::Byte>(18)),
+				static_cast<System::Int32>(static_cast<System::Byte>(18)));
+			this->pnlSubMenu->Controls->Add(this->button5);
+			this->pnlSubMenu->Controls->Add(this->button4);
+			this->pnlSubMenu->Controls->Add(this->button3);
+			this->pnlSubMenu->Controls->Add(this->button2);
+			this->pnlSubMenu->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnlSubMenu->Location = System::Drawing::Point(0, 197);
+			this->pnlSubMenu->Name = L"pnlSubMenu";
+			this->pnlSubMenu->Size = System::Drawing::Size(250, 196);
+			this->pnlSubMenu->TabIndex = 1;
+			// 
+			// button5
+			// 
+			this->button5->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button5->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->Location = System::Drawing::Point(0, 120);
+			this->button5->Name = L"button5";
+			this->button5->Padding = System::Windows::Forms::Padding(35, 0, 0, 0);
+			this->button5->Size = System::Drawing::Size(250, 40);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"button5";
+			this->button5->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button4->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->Location = System::Drawing::Point(0, 80);
+			this->button4->Name = L"button4";
+			this->button4->Padding = System::Windows::Forms::Padding(35, 0, 0, 0);
+			this->button4->Size = System::Drawing::Size(250, 40);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"button4";
+			this->button4->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button3->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Location = System::Drawing::Point(0, 40);
+			this->button3->Name = L"button3";
+			this->button3->Padding = System::Windows::Forms::Padding(35, 0, 0, 0);
+			this->button3->Size = System::Drawing::Size(250, 40);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"button3";
+			this->button3->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button2->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(0, 0);
+			this->button2->Name = L"button2";
+			this->button2->Padding = System::Windows::Forms::Padding(35, 0, 0, 0);
+			this->button2->Size = System::Drawing::Size(250, 40);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// btnMedios
+			// 
+			this->btnMedios->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(187)), static_cast<System::Int32>(static_cast<System::Byte>(134)),
+				static_cast<System::Int32>(static_cast<System::Byte>(252)));
+			this->btnMedios->Dock = System::Windows::Forms::DockStyle::Top;
+			this->btnMedios->FlatAppearance->BorderSize = 0;
+			this->btnMedios->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->btnMedios->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(179)));
+			this->btnMedios->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnMedios->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnMedios->ForeColor = System::Drawing::Color::White;
+			this->btnMedios->Location = System::Drawing::Point(0, 147);
+			this->btnMedios->Name = L"btnMedios";
+			this->btnMedios->Padding = System::Windows::Forms::Padding(10, 0, 0, 0);
+			this->btnMedios->Size = System::Drawing::Size(250, 50);
+			this->btnMedios->TabIndex = 1;
+			this->btnMedios->Text = L"Medios";
+			this->btnMedios->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->btnMedios->UseVisualStyleBackColor = false;
+			// 
+			// pnlLogo
+			// 
+			this->pnlLogo->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->pnlLogo->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pnlLogo.BackgroundImage")));
+			this->pnlLogo->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pnlLogo->Location = System::Drawing::Point(0, 0);
+			this->pnlLogo->Name = L"pnlLogo";
+			this->pnlLogo->Size = System::Drawing::Size(250, 147);
+			this->pnlLogo->TabIndex = 0;
+			// 
+			// pnlReproductor
+			// 
+			this->pnlReproductor->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->pnlReproductor->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->pnlReproductor->Location = System::Drawing::Point(250, 520);
+			this->pnlReproductor->Name = L"pnlReproductor";
+			this->pnlReproductor->Size = System::Drawing::Size(1075, 129);
+			this->pnlReproductor->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Dubai", 24, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(277, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(319, 67);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Editor de Playlist";
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(1133, 163);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(120, 340);
+			this->listBox1->TabIndex = 3;
+			// 
+			// listBox2
+			// 
+			this->listBox2->FormattingEnabled = true;
+			this->listBox2->ItemHeight = 16;
+			this->listBox2->Location = System::Drawing::Point(304, 147);
+			this->listBox2->Name = L"listBox2";
+			this->listBox2->Size = System::Drawing::Size(120, 356);
+			this->listBox2->TabIndex = 4;
 			// 
 			// FrontEnd
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::Info;
-			this->ClientSize = System::Drawing::Size(1095, 754);
+			this->BackColor = System::Drawing::Color::DimGray;
+			this->ClientSize = System::Drawing::Size(1325, 649);
+			this->Controls->Add(this->listBox2);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pnlReproductor);
+			this->Controls->Add(this->pnlLateral);
 			this->Name = L"FrontEnd";
-			this->Text = L"FrontEnd";
+			this->Opacity = 0.98;
+			this->ShowIcon = false;
+			this->Text = L"Music Festival";
+			this->pnlLateral->ResumeLayout(false);
+			this->pnlSubMenu->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
